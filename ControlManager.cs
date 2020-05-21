f7c9fe9aba228a04c6817ebc67bb9f4259ba73b1
ROUANE Anis <35432598+ZaPuncakeu@users.noreply.github.com>
2020-05-21 01:56:09 +0100

    Add files via upload

---
+++ ControlManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlManager : MonoBehaviour
+{
+    [SerializeField]
+    private bool isKeyBoard = true;
+
+    [SerializeField]
+    private string[] inputNames = new string[4];
+
+    [SerializeField]
+    private KeyCode[] keyboard = new KeyCode[4];
+
+    [SerializeField]
+    private KeyCode[] controller = new KeyCode[4];
+
+    public int GetAxis(string axis)
+    {
+        if(axis == "Horizontal")
+        {
+            if(isKeyBoard)
+            {
+                if(Input.GetKey(keyboard[0]))
+                {
+                    return 1;
+                }
+
+                if(Input.GetKey(keyboard[1]))
+                {
+                    return -1;
+                }
+
+                return 0;
+            }
+            else 
+            {
+                if(Input.GetKey(controller[0]))
+                {
+                    return 1;
+                }
+
+                if(Input.GetKey(controller[1]))
+                {
+                    return -1;
+                }
+
+                return 0;
+            }
+        }
+
+        return 0;
+    }
+
+    public bool GetButtonDown(int index)
+    {
+        if(isKeyBoard)
+        {
+            return Input.GetKeyDown(keyboard[index]);
+        }
+        else 
+        {
+            return Input.GetKeyDown(controller[index]);
+        }
+
+        return false;
+    }
+}
+
+/* 
+    0 -> right
+    1 -> left
+    2 -> jump
+*/
---
+++ PlayerController.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float movespeed;
+
+    [SerializeField]
+    private ControlManager cmn;
+
+    [SerializeField]
+    private bool lookingRight = true; 
+
+    private float horizontal;
+
+    private Rigidbody2D prb;
+
+    private Animator pan;
+
+    private bool afk = false;
+
+    private bool crouched = false;
+
+    [SerializeField]
+    private LayerMask groundType;
+
+    [SerializeField]
+    private GameObject groundChecker;
+
+    [SerializeField]
+    private float radiusGroundCheck;
+
+    [SerializeField]
+    private float jumpHeight;
+
+    private void Start()
+    {
+        prb = GetComponent<Rigidbody2D>();
+        pan = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Crouch();
+        Jump();
+        Run();
+        AnimationManager();
+    }
+
+    private void AnimationManager()
+    {
+        pan.SetFloat("movespeed", Mathf.Abs(horizontal));
+        pan.SetBool("afk", afk);
+        pan.SetBool("grounded", Grounded());
+        pan.SetFloat("jump idle", prb.velocity.y);
+        pan.SetBool("crouched", crouched);
+    }
+
+    private void Run()
+    {
+        horizontal = cmn.GetAxis("Horizontal");
+        if(!afk)
+        {
+            prb.velocity = new Vector2(movespeed * horizontal, prb.velocity.y);
+        }
+        else 
+        {
+            if(horizontal != 0)
+            {
+                pan.SetTrigger("btk");
+            }
+        }
+
+        if((horizontal > 0 && !lookingRight) || (horizontal < 0 && lookingRight))
+        {
+            Flip();
+        }
+    }
+
+    private void Jump()
+    {
+        if(Grounded())
+        {
+            if(cmn.GetButtonDown(2))
+            {
+                crouched = false;
+                prb.velocity = new Vector2(prb.velocity.x, jumpHeight);
+            }
+        }
+    }
+
+    private void Crouch()
+    {
+        if(cmn.GetButtonDown(3) && horizontal == 0)
+        {
+            if(!crouched)
+            {
+                pan.SetTrigger("s2c");
+            }
+            else 
+            {
+                pan.SetTrigger("c2s");
+            }
+        }
+    }
+
+    private void SetAfk()
+    {
+        afk = !afk;
+    }
+
+    private void SetCrouch()
+    {
+        crouched = !crouched;
+    }
+
+    private void Flip()
+    {
+        Vector3 lscale = transform.localScale;
+        lscale.x *= -1;
+        transform.localScale = lscale;
+        lookingRight = !lookingRight;
+    }
+
+    public bool Grounded()
+    {
+        return Physics2D.OverlapCircle(groundChecker.transform.position,
+                                       radiusGroundCheck, groundType);
+    }
+}
